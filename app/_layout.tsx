1c98d5318b74b20ca10fb38b7eecc6367ee83db5
vandamd <vandamdinh71@gmail.com>
2025-06-20 22:31:58 +0100

    hide navbar and status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect } from "react";
 import { Stack } from "expo-router";
 import { HapticProvider } from "../contexts/HapticContext";
-import { InvertColorsProvider } from "../contexts/InvertColorsContext";
 import { useFonts } from "expo-font";
 import { setStatusBarHidden } from "expo-status-bar";
+import {
+	InvertColorsProvider,
+	useInvertColors,
+} from "@/contexts/InvertColorsContext";
+import * as SystemUI from "expo-system-ui";
+import * as NavigationBar from 'expo-navigation-bar';
+
 
 function RootNavigation() {
 	useFonts({
 		"PublicSans-Regular": require("../assets/fonts/PublicSans-Regular.ttf"),
 	});
 
+    const { invertColors } = useInvertColors();
 	useEffect(() => {
 		setStatusBarHidden(true, "none");
-	}, []);
+        const newColor = invertColors ? "#FFFFFF" : "#000000";
+		SystemUI.setBackgroundColorAsync(newColor);
+
+	}, [invertColors]);
+
+    NavigationBar.setVisibilityAsync("hidden");
 
 	return (
 		<Stack

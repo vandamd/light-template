e07350e8402919c5f80d874e3d98ec139f8470bf
vandamd <vandamdinh71@gmail.com>
2026-03-30 23:13:43 +0100

    cleanup font loading and splashscreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,11 @@
-import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
 import { HapticProvider } from "@/contexts/HapticContext";
 import { InvertColorsProvider, useInvertColors } from "@/contexts/InvertColorsContext";
 import { DisplayModeProvider } from "@/contexts/DisplayModeContext";
 
 function RootLayout() {
     const { invertColors } = useInvertColors();
-
-    const [fontsLoaded, fontError] = useFonts({
-        "PublicSans-Regular": require("../assets/fonts/PublicSans-Regular.ttf"),
-    });
-
-    useEffect(() => {
-        if (fontsLoaded || fontError) {
-            SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded, fontError]);
-
-    if (!fontsLoaded && !fontError) {
-        return null;
-    }
 
     return (
         <Stack
